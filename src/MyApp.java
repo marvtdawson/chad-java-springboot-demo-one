0dbe483207612d4da69cf21dd7de2c889f65f4f4
Marvin Dawson <marvintdawson@gmail.com>
2018-12-22 22:03:49 -0600

    first init commit

---
+++ src/MyApp.java
@@ -0,0 +1,11 @@
+public class MyApp {
+
+    public static void main(String[] args) {
+        // create the object
+
+        BaseballCoach theCoach = new BaseballCoach();
+
+        // use the object
+        System.out.println(theCoach.getDailyWorkout());
+    }
+}
